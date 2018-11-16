a224c0d9de247586cda027461fe3ce3965b23f21
belov <vladislav.belov@merck.com>
2018-11-16 19:13:55 +0100

    cross-validation of the random forest

--- Titanic/main.py
+++ Titanic/main.py
@@ -5,9 +5,22 @@ from Titanic.models.random_forest import *
 # Load the data (both train and test sets)
 train, test = load_data(process=True)
 
+# Cross-validation
+# best_params = cross_validate_randomized(train, label="Survived")
+# best_params = cross_validate_grid(train, label="Survived")
+
+params = {
+    'bootstrap': True,
+    'max_depth': 5,
+    'max_features': 3,
+    'min_samples_leaf': 3,
+    'min_samples_split': 2,
+    'n_estimators': 475
+}
+
 # Train the model
 model = train_model(train.drop(["PassengerId"], axis=1),
-                    label="Survived", test=False)
+                    label="Survived", params=params, test=False)
 
 # Make predictions
 submission_df = predict_and_format(model=model, test_set=test,
@@ -15,4 +28,4 @@ submission_df = predict_and_format(model=model, test_set=test,
                                    predicted_colname="Survived")
 
 # Save the submission file
-submission_df.to_csv('.\\all\\titanic_predicted.csv', index=False)
+submission_df.to_csv('.\\data\\titanic_predicted.csv', index=False)
--- Titanic/models/random_forest.py
+++ Titanic/models/random_forest.py
@@ -3,9 +3,83 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 
 
-def train_model(df: pd.DataFrame, label: str, test=False):
+def cross_validate_randomized(df: pd.DataFrame, label: str):
+
+    # Separate data into labels and features
+    labels = np.asarray(df[label])
+    features = df.drop(label, axis=1)
+    features = np.asarray(features)
+
+    # Number of trees in random forest
+    n_estimators = [int(x) for x in np.linspace(start=10, stop=1000, num=10)]
+    # Number of features to consider at every split
+    max_features = ['auto', 'sqrt']
+    # Maximum number of levels in tree
+    max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
+    # noinspection PyTypeChecker
+    max_depth.append(None)
+    # Minimum number of samples required to split a node
+    min_samples_split = [2, 5, 10]
+    # Minimum number of samples required at each leaf node
+    min_samples_leaf = [1, 2, 4]
+    # Method of selecting samples for training each tree
+    bootstrap = [True, False]
+    # Create the random grid
+    random_grid = {'n_estimators': n_estimators,
+                   'max_features': max_features,
+                   'max_depth': max_depth,
+                   'min_samples_split': min_samples_split,
+                   'min_samples_leaf': min_samples_leaf,
+                   'bootstrap': bootstrap}
+
+    rf = RandomForestClassifier()
+    # Random search of parameters, using 3 fold cross validation,
+    # search across 100 different combinations, and use all available cores
+    rf_random = RandomizedSearchCV(estimator=rf,
+                                   param_distributions=random_grid,
+                                   n_iter=100, cv=3, verbose=2,
+                                   random_state=123, n_jobs=-1)
+    # Fit the random search model
+    rf_random.fit(features, labels)
+
+    return rf_random.best_params_
+
+
+def cross_validate_grid(df: pd.DataFrame, label: str):
+
+    # Separate data into labels and features
+    labels = np.asarray(df[label])
+    features = df.drop(label, axis=1)
+    features = np.asarray(features)
+
+    # Create the parameter grid based on the results of random search
+    param_grid = {
+        'bootstrap': [True],
+        'max_depth': [i for i in range(5, 12, 1)],
+        'max_features': [2, 3],
+        'min_samples_leaf': [i for i in range(3, 7, 1)],
+        'min_samples_split': [2, 3, 4],
+        'n_estimators': [int(x) for x in np.linspace(start=475, stop=625,
+                                                     num=20)]
+    }
+
+    # Create a based model
+    rf = RandomForestClassifier()
+    # Instantiate the grid search model
+    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid,
+                               cv=3, n_jobs=-1, verbose=2)
+
+    # Fit the grid search to the data
+    grid_search.fit(features, labels)
+
+    return grid_search.best_params_
+
+
+def train_model(df: pd.DataFrame, label: str, params: dict, test=False):
 
     # Separate data into labels and features
     labels = np.asarray(df[label])
@@ -15,7 +89,7 @@ def train_model(df: pd.DataFrame, label: str, test=False):
     # Split data into train and test set if required
     if test:
         train_features, test_features, train_labels, test_labels = \
-            train_test_split(features, labels, test_size=0.25, random_state=123)
+            train_test_split(features, labels, test_size=0.2, random_state=123)
     else:
         train_features = features
         train_labels = labels
@@ -23,8 +97,7 @@ def train_model(df: pd.DataFrame, label: str, test=False):
         test_labels = []
 
     # Instantiate model with 1000 decision trees
-    rf = RandomForestClassifier(n_estimators=10000, random_state=123,
-                                warm_start=True, max_depth=25)
+    rf = RandomForestClassifier(**params)
 
     # Train the model
     print("Training the random forest model...")
