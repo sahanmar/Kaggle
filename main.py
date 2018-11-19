7274ad17955178c5468c4c1763f720f2b2a5ef56
Sahan <msahan@deloittece.com>
2018-11-19 15:56:36 +0100

    refactoring

--- main.py
+++ main.py
@@ -7,29 +7,44 @@ This is a temporary script file.
 
 import pandas as pd
 import data_cleaner as dc
-import numpy as np
+import models
 
 
 
 if __name__=='__main__':
 
     #Load data to pandas Dataframe
-    train_csv = pd.read_csv(r'C:\Users\msahan\Documents\Python Scripts\Kaggle\Titanic\all\train.csv')
-    test_csv = pd.read_csv(r'C:\Users\msahan\Documents\Python Scripts\Kaggle\Titanic\all\test.csv')
+    train_csv = pd.read_csv(r'.\all\train.csv')
+    test_csv = pd.read_csv(r'.\all\test.csv')
     
     #Get training labels 
     train_labels = train_csv.get_values()[:,1]
     
     #Drop unwanted columns
-    train_csv = train_csv.drop(['Survived','PassengerId'], axis=1)
-    test_csv = test_csv.drop('PassengerId', axis=1)
+    train_csv = train_csv.drop(['Survived', 'PassengerId', 'Ticket', 'Cabin' ], axis=1)
+    test_csv = test_csv.drop(['PassengerId', 'Ticket', 'Cabin'], axis=1)
+    
+    
     
     #Union data
     union_data = pd.concat([train_csv,test_csv])
     
     
-    
+    #Clean dataset
     data = dc.clean_data(union_data)
+    
+    #Transform to numpy format
+    data_numpy = data.get_values()
+    
+    #Split back to train and test 
+    train_data = data_numpy[:len(train_csv),:]
+    test_data = data_numpy[len(train_csv):,:]
+    
+    #K-fold cross validation  
+    results = models.k_fold_cross_valid(train_data, train_labels, 10)
+    
+    #Create submission documents
+    models.submission_doc(train_data, train_labels, test_data)
     
     
     
---
+++ models.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 19 10:16:23 2018
+
+@author: msahan
+"""
+
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import KFold
+from mpl_toolkits.mplot3d import Axes3D
+
+from sklearn.ensemble import RandomForestClassifier , AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier, VotingClassifier
+from sklearn.datasets import make_classification
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn import svm
+from sklearn import tree, model_selection
+
+
+from xgboost import XGBClassifier
+
+
+
+
+
+def k_fold_cross_valid(data: np.ndarray, labels: np.ndarray, split: int):
+    
+    kf = KFold(n_splits=split)
+    kf.get_n_splits(data)
+        
+    data = data.astype('float')
+    labels = labels.astype('float')
+    
+    scores_array = []
+    
+    '''
+    
+    param_grid = {'criterion': ['gini', 'entropy'], 
+                  'max_depth': [2,4,6,8,10,None],   
+                  #'min_samples_split': [2,5,10,.03,.05], #minimum subset size BEFORE new split (fraction is % of total); default is 2
+                  #'min_samples_leaf': [1,5,10,.03,.05], #minimum subset size AFTER new split split (fraction is % of total); default is 1
+                  #'max_features': [None, 'auto'], #max features to consider when performing split; default none or all
+                  'random_state': [0] #seed or control random number generator: https://www.quora.com/What-is-seed-in-random-number-generation
+                  }
+    
+    
+    tune_model = model_selection.GridSearchCV(RandomForestClassifier(), param_grid=param_grid, scoring = 'roc_auc', cv = split)
+    tune_model.fit(data, labels)
+        
+    print('AFTER DT Parameters: ', tune_model.best_params_)
+    #print(tune_model.cv_results_['mean_train_score'])
+    print("AFTER DT Training w/bin score mean: {:.2f}". format(tune_model.cv_results_['mean_train_score'][tune_model.best_index_]*100)) 
+    #print(tune_model.cv_results_['mean_test_score'])
+    print("AFTER DT Test w/bin score mean: {:.2f}". format(tune_model.cv_results_['mean_test_score'][tune_model.best_index_]*100))
+    print("AFTER DT Test w/bin score 3*std: +/- {:.2f}". format(tune_model.cv_results_['std_test_score'][tune_model.best_index_]*100*3))
+    print('-'*10)
+    
+    '''
+    for train_index, test_index in kf.split(data):
+            X_train, X_test = data[train_index], data[test_index]
+            y_train, y_test = labels[train_index], labels[test_index]  
+            #predicted, scores = knn.feed_forward_NN(X_train,y_train, X_test, y_test)
+            #scores_array.append(scores)
+            
+            clf = RandomForestClassifier(criterion = 'entropy', 
+                                 max_depth = 6,
+                                 n_estimators = 100,
+                                 oob_score = True,
+                                 random_state = 0)
+
+            clf.fit(X_train, y_train)
+            y_pred = clf.predict(X_test)
+            scores_array.append(accuracy_score(y_test, y_pred))
+            
+            #clf = AdaBoostClassifier()
+            #clf.fit(X_train, y_train)
+            #y_pred = clf.predict(X_test)
+            #scores_array.append(accuracy_score(y_test, y_pred))
+            
+            #clf = ExtraTreesClassifier()
+            #clf.fit(X_train, y_train)
+            #y_pred = clf.predict(X_test)
+            #scores_array.append(accuracy_score(y_test, y_pred))
+            
+            #clf = GradientBoostingClassifier()
+            #clf.fit(X_train, y_train)
+            #y_pred = clf.predict(X_test)
+            #scores_array.append(accuracy_score(y_test, y_pred))
+            
+            #clf = VotingClassifier()
+            #clf.fit(X_train, y_train)
+            #y_pred = clf.predict(X_test)
+            #scores_array.append(accuracy_score(y_test, y_pred))
+            
+            #clf = GaussianNB()
+            #clf.fit(X_train, y_train)
+            #y_pred = clf.predict(X_test)
+            #scores_array.append(accuracy_score(y_test, y_pred))
+            
+            #model = XGBClassifier()
+            #model.fit(X_train, y_train)
+            #y_pred = model.predict(X_test)
+            #predictions = [round(value) for value in y_pred]
+            #scores_array.append(accuracy_score(y_test, predictions))
+            
+            #clf = svm.SVC(decision_function_shape='ovo')
+            #clf.fit(X_train, y_train) 
+            #y_pred = clf.predict(X_test)
+            #scores_array.append(accuracy_score(y_test, y_pred))
+            
+   
+    print(np.mean(scores_array))    
+    
+     
+    
+def submission_doc(train_data: np.ndarray, labels: np.ndarray, test_data: np.ndarray):  
+    
+    train_data = train_data.astype('float')
+    test_data = test_data.astype('float')
+    labels = labels.astype('float')
+    
+    #Optimal parameters from the function above 
+    clf = RandomForestClassifier(criterion = 'entropy', 
+                                 max_depth = 6,
+                                 n_estimators = 100,
+                                 oob_score = True,
+                                 random_state = 0)
+    
+    clf.fit(train_data, labels)
+    
+    predictions = clf.predict(test_data).astype(int)
+    
+    test_csv = pd.read_csv('./all/test.csv')
+    submission = pd.DataFrame({'PassengerId':test_csv['PassengerId'],'Survived':predictions})   
+    
+    filename = 'Titanic_Predictions.csv'
+
+    submission.to_csv(filename,index=False)
+    
